30ba8ef18dedf1c3fcc1ac5235626dc5a088643f
Noah Cowan <nsncowan@gmail.com>
2022-09-13 19:42:56 -0700

    add delete function and additional display function.
    Co-authored by: Remy Flores
    <remyfranciscoflores@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -30,12 +30,12 @@ AddressBook.prototype.deleteContact = function(id) {
 };
 
 // Business Logic for Contacts ---------
-function Contact(firstName, lastName, phoneNumber, emailAddress, homeAddress) {
+function Contact(firstName, lastName, phoneNumber, /*emailAddress, homeAddress*/) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.phoneNumber = phoneNumber;
-  this.emailAddress = emailAddress;
-  this.homeAddress = homeAddress;
+  //this.emailAddress = emailAddress;
+  //this.homeAddress = homeAddress;
 }
 
 Contact.prototype.theName = function() {
@@ -46,13 +46,13 @@ Contact.prototype.thePhone = function() {
   return this.phoneNumber;
 };
 
-Contact.prototype.theEmail = function() {
+/*Contact.prototype.theEmail = function() {
   return this.emailAddress;
 };
 
 Contact.prototype.theHome = function() {
   return this.homeAddress;
-};
+};*/
 
 
 // User Interface Logic ---------
@@ -66,20 +66,20 @@ function listContacts(addressBookToDisplay) {
     const contact = addressBookToDisplay.findContact(key);
     const li = document.createElement("li");
     const li2 = document.createElement("li");
-    const li3 = document.createElement("li");
-    const li4 = document.createElement("li");
+    //const li3 = document.createElement("li");
+    //const li4 = document.createElement("li");
     li.append(contact.theName());
     li2.append(contact.thePhone());
-    li3.append(contact.theEmail());
-    li4.append(contact.theHome());
+    //li3.append(contact.theEmail());
+    //li4.append(contact.theHome());
     li.setAttribute("id", contact.id);
     li2.setAttribute("id", contact.id);
-    li3.setAttribute("id", contact.id);
-    li4.setAttribute("id", contact.id);
+    //li3.setAttribute("id", contact.id);
+    //li4.setAttribute("id", contact.id);
     ul.append(li);
     ul.append(li2);
-    ul.append(li3);
-    ul.append(li4);
+    //ul.append(li3);
+    //ul.append(li4);
   });
   contactsDiv.append(ul);
 }
@@ -89,13 +89,35 @@ function handleFormSubmission(event) {
   const inputtedFirstName = document.querySelector("input#new-first-name").value;
   const inputtedLastName = document.querySelector("input#new-last-name").value;
   const inputtedPhoneNumber = document.querySelector("input#new-phone-number").value;
-  const inputtedEmailAddress = document.querySelector("input#emailAddress").value;
-  const inputtedHomeAddress = document.querySelector("input#homeAddress").value;
-  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, inputtedEmailAddress, inputtedHomeAddress);
+ // const inputtedEmailAddress = document.querySelector("input#emailAddress").value;
+// const inputtedHomeAddress = document.querySelector("input#homeAddress").value;
+  let newContact = new Contact(inputtedFirstName, inputtedLastName, inputtedPhoneNumber, /*inputtedEmailAddress, inputtedHomeAddress*/);
   addressBook.addContact(newContact);
   listContacts(addressBook);
-}
+  document.querySelector("input#new-first-name").value = null;
+  document.querySelector("input#new-last-name").value = null;
+  document.querySelector("input#new-phone-number").value = null;
+};
+
+function displayContactDetails(event) {
+  const contact = addressBook.findContact(event.target.id);
+  document.getElementById("first-name1").innerText = contact.firstName;
+  document.getElementById("last-name1").innerText = contact.lastName;
+  document.getElementById("phone-number1").innerText = contact.phoneNumber;
+  document.querySelector("div#contact-details").removeAttribute("class");
+  document.querySelector("button.delete").setAttribute("id", contact.id);
+  document.querySelector("div#contact-details").removeAttribute("class");
+};
+
+function handleDelete(event) {
+  addressBook.deleteContact(event.target.id);
+  document.querySelector("button.delete").removeAttribute("id");
+  document.querySelector("div#contact-details").setAttribute("class", "hidden");
+  listContacts(addressBook);
+};
 
 window.addEventListener("load", function (){
   document.querySelector("form#new-contact").addEventListener("submit", handleFormSubmission);
+  document.querySelector("div#contacts").addEventListener("click", displayContactDetails);
+  document.querySelector("button.delete").addEventListener("click", handleDelete);
 });
